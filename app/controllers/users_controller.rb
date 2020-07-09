132f549b150d4ac016b3b47d57a8de17af5e8174
shinichiroh nishijima <hgnsuika@gmail.com>
2020-07-09 06:16:52 +0000

    add heroku

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -42,7 +42,7 @@ class UsersController < ApplicationController
 
   def likes
     @user = User.find(params[:id])
-    @microposts = @user.microposts.page(params[:page])
+    @favorites = @user.favorites.order(id: :desc).page(params[:page])
     counts(@user)
   end
    
